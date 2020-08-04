e113e2fba2d9ae21e6e3c22873763c1574e4a15a
rmiller003 <54287828+rmiller003@users.noreply.github.com>
2020-08-04 17:43:56 -0400

    1st chunk

--- Tetrominoes.py
+++ Tetrominoes.py
@@ -138,12 +138,23 @@ shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 16
 
 
 class Piece(object):
-    pass
+    def_init_(self, x, y,shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.color = shape_colors[shapes.index(shape)]
+        self.rotation = 0
 
 
 def create_grid(locked_positions={}):
-    pass
+    grid = [[(0,0,0) for _ in range(10)] for _ in range(20)]
 
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (j,i) in locked_pos:
+                c = locked_pos[(j,i)]
+                grid[i][j] = c
+    return grid
 
 def convert_shape_format(shape):
     pass
@@ -158,15 +169,19 @@ def check_lost(positions):
 
 
 def get_shape():
-    pass
+    return random.choice(shapes)
 
 
 def draw_text_middle(text, size, color, surface):
     pass
 
 
-def draw_grid(surface, row, col):
-    pass
+def draw_grid(surface, grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            pygame.draw.rect(surface, grid[i][j], (top_left_x + j*block_size, top_left_y+ i*block_size, block_size, block_size), 0)
+
+    pygame.draw.rect(surface, (255,0,0), (top_left_x, top_left_y, play_width, play_height, 4)
 
 
 def clear_rows(grid, locked):
@@ -175,16 +190,50 @@ def clear_rows(grid, locked):
 def draw_next_shape(shape, surface):
 
 
-def draw_window(surface):
-    pass
+def draw_window(surface, grid):
+    surface.fill((0,0,0))
 
+    pygame.font.init()
+    font = pygame.font.sysFont('comicsans', 60)
+    label = font.render('Tetrominos', 1, (255,255,255))
+
+    surface.blitz(label, (top_left_x + play_width/2 - (label.get_width()/2), 30))
+
+    draw_grid(surface, grid)
+    pygame.display.update()
 
 def main():
-    pass
+
+    locked_positions = {}
+    grid = create_grid(locked_positions)
+
+    change piece = False
+    run = True
+    current piece = False
+    next_piece = get_shape()
+    clock = pygame.time.Clock()
+    fall_time = 0
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    current_piece.x -= 1
+                if event.key == pygame.K_RIGHT:
+
+                if event.key == pygame.K_DOWN:
+
+                if event.key == pygame.K_UP:
+
+
 
 
 def main_menu():
-    pass
+
+    locked positions =
 
 
 main_menu()  # start game
