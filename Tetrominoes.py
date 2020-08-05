137d5eb5bbe9f2bc5498bbcb0ca126ead14e0ff5
rmiller003 <54287828+rmiller003@users.noreply.github.com>
2020-08-04 21:32:46 -0400

    1st chunk 2.0

--- Tetrominoes.py
+++ Tetrominoes.py
@@ -169,7 +169,7 @@ def check_lost(positions):
 
 
 def get_shape():
-    return random.choice(shapes)
+    return Piece(5, 0, random.choice(shapes))
 
 
 def draw_text_middle(text, size, color, surface):
@@ -202,7 +202,7 @@ def draw_window(surface, grid):
     draw_grid(surface, grid)
     pygame.display.update()
 
-def main():
+def main(win):
 
     locked_positions = {}
     grid = create_grid(locked_positions)
@@ -222,18 +222,28 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     current_piece.x -= 1
+                    if not(valid_space(current_piece, grid)):
+                        current_piece += 1
                 if event.key == pygame.K_RIGHT:
-
+                    current_piece.x += 1
+                    if not(valid_space(current_piece), grid):
+                        current_piece -= 1
                 if event.key == pygame.K_DOWN:
-
+                    current_piece.y += 1
+                    if not(valid_space(current_piece, grid)):
+                        current_piece.y
                 if event.key == pygame.K_UP:
+                    current_piece.rotation += 1
+                    if not(valid_space(current_piece, grid)):
+                        current_piece -= 1
+
+        draw_window(win, grid)
 
 
 
+def main_menu(win):
+    main(win)
 
-def main_menu():
-
-    locked positions =
-
-
-main_menu()  # start game
+win = pygame.display.set_mode((s_width, s_height))
+pygame.display.set_caption('Tetrominose')
+main_menu(win)  # start game
